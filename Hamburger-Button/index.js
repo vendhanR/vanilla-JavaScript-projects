fe249e161747d25c3f37e816c1f1346bf338b8f4
vendhan <vendhan7799@gmail.com>
2023-12-05 22:27:24 +0530

    Added hamburger-button projects

---
+++ Hamburger-Button/index.js
@@ -0,0 +1,7 @@
+const btnEl = document.getElementById("btn");
+const navEl = document.getElementById("nav");
+
+btnEl.addEventListener("click", () => {
+    console.log("dskms")
+    navEl.classList.toggle("active")
+})
